24f3e566e46f806ed7645c219423827359d0a684
Agas17 <anggorogigis9191@gmail.com>
2025-09-21 23:03:41 +0700

    assasin

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,44 +1,103 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime;
+using System.Runtime.CompilerServices;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        fighter fighter1 = new fighter();
-        fighter1.name = "Al-Haytam";
-        fighter1.str = 20;
-        fighter1.level = "10";
+        Character char1 = new Character("richard");
+        char1.introduction();
 
-        Console.Write($"{fighter1.profil()}");
+        Assasin char2 = new Assasin("Altair", "hidden blade");
+        char2.introduction(false);
+        Assasin char3 = new Assasin("connor", "hidden blade", new skill("axe"));
+        char3.introduction(true);
 
-        fighter1.LevelUp(25, "11");
     }
 
-}
-public class fighter
-{
-    private string name;
-    public int str;
-    public string level;
+    public class Character
+    {
+        public string name;
+        private int health;
 
-    private string gold;
-    private bool online;
-    public class GetName()
-    {
-        return name;
-    }
-    public string profil()
-    {
-        return $"saya : {name} \nStrength : {str}\n";
-    }
-    public void LevelUp(int str, string level)
-    {
-        this.str = str;
-        this.level = level;
+        public Character(string name)
+        {
+            this.name = name;
+            this.health = 100;
+        }
 
-        Console.WriteLine("Selamat, anda naik level!!");
-        Console.WriteLine($"Strength : {str}");
-        Console.WriteLine($"Level    : {level}");
+
+        public int getHealth()
+        {
+            return this.health;
+        }
+
+        public void introduction()
+        {
+            Console.WriteLine("\nCharacter" +
+                              "\nName   : " + this.name +
+                              "\nhealth : "+ this.health);
+            principle();
+        }
+
+        public virtual void principle()
+        {
+            Console.WriteLine("My principle is netral");
+        }
+    }
+
+    public class skill
+    {
+        public string nameSkill;
+        public skill(string nameSkill)
+        {
+            this.nameSkill = nameSkill;
+        }
+    }
+
+    public class Assasin : Character
+    {
+        private string weapon;
+        private skill skill;
+        public Assasin(string name, string weapon, skill skill = null) : base(name)
+        {
+            this.weapon = weapon;
+            this.skill = skill;
+        }
+
+        public void introduction(bool showWeapon)
+        {
+            if (showWeapon)
+            {
+                Console.Write("\nAssassin" +
+                              "\nName   : " + this.name +
+                              "\nHealth : " + getHealth() +
+                              "\nWeapon : " + this.weapon);
+                if (skill != null)
+                {
+                    Console.WriteLine("\nSKill  : " + this.skill.nameSkill);
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nAssassin" +
+                              "\nName   : " + this.name +
+                              "\nHealth : " + getHealth());
+            }
+
+            principle();
+        }
+
+        public override void principle()
+        {
+            Console.WriteLine("My principle is freedom");
+        }
+
     }
 }
